788383259769c05383bb42201fdfc32ac4e2ede6
Mehdi Radfar <metiii.py@gmail.com>
2025-04-06 15:00:35 +0330

    Config - 🌠 Updated settings for stop hard coding for MAX_TRIES 🌠

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'channels',
+    'rest_framework_simplejwt',
+    
+    'accounts',
+    'profiles',
+    'messagings',
+    'backups',
+    'sessions',
+    'friendships',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MAX_OTP_REFRESH = 3
+MAX_AUTH_TRIES = 5
+
+AUTH_USER_MODEL = "accounts.User"
