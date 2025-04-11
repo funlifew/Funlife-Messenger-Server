2ac9cc324843ee2395e2cef71c0e0b8f1b0f2fc8
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-11 13:16:53 +0330

    Profile - Views - 💯 Create views of Profile 💯

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import permissions
+from .serializers import ProfileSerializer
+from django.utils import timezone
 
 # Create your views here.
+class ProfileView(APIView):
+    """User's Profile showing up"""
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get(self, request):
+        serializer = ProfileSerializer()
+        return Response()
