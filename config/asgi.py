220e823a34a5c979d0cd68cbb16db94d75029885
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-19 12:00:56 +0330

    Config - ⚙️ Customized ASGI

--- config/asgi.py
+++ config/asgi.py
@@ -1,16 +1,25 @@
 """
 ASGI config for config project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/asgi/
 """
 
 import os
+import django
 
-from django.core.asgi import get_asgi_application
-
+# Set the Django settings module before importing any other Django modules
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+django.setup()  # <-- Add this line to configure Django settings
 
-application = get_asgi_application()
+# Now import the rest of your modules
+from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.auth import AuthMiddlewareStack
+import messagings.routing
+
+application = ProtocolTypeRouter({
+    "http": get_asgi_application(),
+    "websocket": AuthMiddlewareStack(
+        URLRouter(
+            messagings.routing.websocket_urlpatterns
+        )
+    ),
+})
