5c8bb176cbad42d629130689779061080d387ebd
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-19 16:54:58 +0330

    Config - ⚙️ Updated ASGI application

--- config/asgi.py
+++ config/asgi.py
@@ -4,22 +4,22 @@ ASGI config for config project.
 
 import os
 import django
+from django.core.asgi import get_asgi_application
 
 # Set the Django settings module before importing any other Django modules
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
-django.setup()  # <-- Add this line to configure Django settings
+django.setup()  # Configure Django settings
 
 # Now import the rest of your modules
-from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from messagings.middlewares import JWTMiddleware
+from channels.security.websocket import AllowedHostsOriginValidator
+from messagings.middlewares import JWTAuthMiddleware
 import messagings.routing
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-    "websocket": AuthMiddlewareStack(
-        JWTMiddleware(
+    "websocket": AllowedHostsOriginValidator(
+        JWTAuthMiddleware(
             URLRouter(
                 messagings.routing.websocket_urlpatterns
             )
