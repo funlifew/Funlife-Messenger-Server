810189ecb68f42d9309105e1323ca85c77a5fc6d
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-25 00:14:23 +0330

    Backup - ⚙️ Add signals when app is ready

--- backups/apps.py
+++ backups/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class BackupsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'backups'
+    
+    def ready(self):
+        """Import signals when app is ready"""
+        import backups.signals
