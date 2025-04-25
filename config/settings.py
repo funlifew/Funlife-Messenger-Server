52687ba313b17f982b880a5873a2127cc8c6171d
Mehdi Radfar <81019265+funlifew@users.noreply.github.com>
2025-04-25 14:49:56 +0330

    Config - ⚙️ Updated DEBUG through the dotenv

--- config/settings.py
+++ config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config("SECRET_KEY", "django-insecure--")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
